4f19968366fae0c710bcec929d872f36fa34f913
Keith J. Jones <keith@keithjjones.com>
2017-06-17 19:36:21 -0400

    Fixed indices.

--- windowentropy.py
+++ windowentropy.py
@@ -8,14 +8,24 @@ parser.add_argument('Size',
                     type=int)
 parser.add_argument("-w", "--window",
                     help="Window size, in bytes, for running entropy."
-                         "", type=int, required=False)
+                         "", type=int, required=True)
 parser.add_argument("-s", "--seed",
                     help="The seed value to the random function to simulate a malware file."
                          "", default=0, type=int, required=False)
 
 args = parser.parse_args()
 
+k = args.Size
+j = args.window
+
 # Create a random file, based upon seed, for the simulated malware file.
 random.seed(args.seed)
-malware = [random.randint(0, 8) for i in range(1, args.Size)]
+malware = [random.randint(0, 7) for i in range(1, k+1)]
 
+print("malware")
+print(malware)
+
+for x in range(1, k-j+2):
+    m = [0 for i in range(0, 8)]
+    for y in range(1, j+1):
+        m[malware[x+y-2]] += 1
