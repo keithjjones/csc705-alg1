92ac611a1373f4505aa08941ce5df31555dd5030
Keith J. Jones <keith@keithjjones.com>
2017-06-17 18:34:58 -0400

    Initial commit.

---
+++ windowentropy.py
@@ -0,0 +1,19 @@
+import argparse
+import random
+
+# Argument parsing
+parser = argparse.ArgumentParser(description='Calculates the entropy of a file.')
+parser.add_argument('Size',
+                    help='The size, in bytes, to simulate a malware file.',
+                    type=int)
+parser.add_argument("-w", "--window",
+                    help="Window size, in bytes, for running entropy."
+                         "", type=int, required=False)
+parser.add_argument("-s", "--seed",
+                    help="The seed value to the random function to simulate a malware file."
+                         "", default=0, type=int, required=False)
+
+args = parser.parse_args()
+
+random.seed(args.seed)
+malware = [random.randint(0, 8) for i in range(1, args.Size)]
