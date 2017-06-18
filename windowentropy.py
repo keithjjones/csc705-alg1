cc1fb23c7af03f9b148547d3774a7b449e586493
Keith J. Jones <keith@keithjjones.com>
2017-06-18 14:01:41 -0400

    Moved scale factor to command line.

--- windowentropy.py
+++ windowentropy.py
@@ -13,16 +13,20 @@ parser.add_argument("-w", "--window",
 parser.add_argument("-s", "--seed",
                     help="The seed value to the random function to simulate a malware file."
                          "", default=0, type=int, required=False)
+parser.add_argument("-K", "--K",
+                    help="The K scale value. Must not be zero."
+                         "", default=1, type=int, required=False)
 
 args = parser.parse_args()
 
 k = args.Size
 j = args.window
+K = args.K
 
 # Create a random file, based upon seed, for the simulated malware file.
 random.seed(args.seed)
 malware = [random.randint(0, 255) for i in range(1, k+1)]
-print(malware)
+print("Malware: {0}".format(malware))
 
 # Entropy list
 H = [0 for i in range(1, k-j+2)]
@@ -34,7 +38,7 @@ for x in range(1, k-j+2):
     entropy = float(0)
     for y in range(0, 256):
         if m[y] != 0:
-            entropy += -(float(m[y]/j) * math.log2(float(m[y]/j)))/8
-    H[x-1] = entropy
+            entropy += -(float(m[y]/j) * math.log2(float(m[y]/j)))
+    H[x-1] = entropy/K
 
-print(H)
+print("Entropy: {0}".format(H))
