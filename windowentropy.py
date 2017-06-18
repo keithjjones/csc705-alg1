402ebf5e609f136752a85f815a2f98583f6b7a72
Keith J. Jones <keith@keithjjones.com>
2017-06-17 20:19:49 -0400

    Fixed up indices.

--- windowentropy.py
+++ windowentropy.py
@@ -1,5 +1,6 @@
 import argparse
 import random
+import math
 
 # Argument parsing
 parser = argparse.ArgumentParser(description='Calculates the entropy of a file.')
@@ -20,12 +21,20 @@ j = args.window
 
 # Create a random file, based upon seed, for the simulated malware file.
 random.seed(args.seed)
-malware = [random.randint(0, 7) for i in range(1, k+1)]
-
-print("malware")
+malware = [random.randint(0, 255) for i in range(1, k+1)]
 print(malware)
 
+# Entropy list
+H = [0 for i in range(1, k-j+2)]
+
 for x in range(1, k-j+2):
-    m = [0 for i in range(0, 8)]
+    m = [0 for i in range(0, 256)]
     for y in range(1, j+1):
         m[malware[x+y-2]] += 1
+    entropy = float(0)
+    for y in range(0, 256):
+        if m[y] != 0:
+            entropy += -(float(m[y]/j) * math.log2(float(m[y]/j)))/8
+    H[x-1] = entropy
+
+print(H)
